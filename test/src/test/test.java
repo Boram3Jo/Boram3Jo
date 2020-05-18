fc4e316e88fc485bcf5251fae4f12585ce9bc458
aiueo <aiueo@Baek>
2020-05-18 22:30:19 +0900

    영!차!영!차!

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,6 +5,11 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("장례식할겁니다");
 		System.out.println("제것 좀 그만합쳐주세요!!");
+		System.out.println("제가 1등입니다.");
+		System.out.println("영");
+		System.out.println("차!");
+		System.out.println("영");
+		System.out.println("차!");
 	}
 
 }
